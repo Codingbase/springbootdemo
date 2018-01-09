9522d57e96664c78e485eab53ba735475fd50f34
shen <88@qq.com>
2018-01-10 01:42:34 +0800

    update

--- src/main/java/com/example/handle/GrilException.java
+++ src/main/java/com/example/handle/GrilException.java
@@ -5,4 +5,10 @@ package com.example.handle;
  * 自定义异常
  */
 public class GrilException {
+    public static void main(String[] args) {
+        System.out.println("helloworld!");
+        String a = "hello";
+        int i = 0;
+        System.out.println(a);
+    }
 }
